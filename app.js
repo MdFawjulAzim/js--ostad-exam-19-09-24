eadea4d3bb3ec788a953e1fb037d44d543d4e33a
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-09-19 22:09:48 +0600

    answer 1

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("Ans to the Qu. No:1");
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+console.log(reverseString("hello"));
+
+console.log("Ans to the Qu. No:2");
