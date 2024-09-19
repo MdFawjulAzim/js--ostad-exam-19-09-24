d2f6a753ca5645dde6ac4215fb36ed8c9decad53
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-09-19 22:10:58 +0600

    ans no 2

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ function reverseString(str) {
 
 console.log(reverseString("hello"));
 
+
 console.log("Ans to the Qu. No:2");
+function sumArray(arr) {
+    return arr.reduce((total, num) => total + num, 0);
+}
+
+console.log(sumArray([1, 2, 3, 4]));
